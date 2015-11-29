d588d7e3eb74e9e405cda6654ee62928c7419cf4
webarata3 <webarata3@gmail.com>
2015-11-29 20:29:29 +0900

    MithrilのAddress追加

---
+++ mithril_test/address/src/app.js
@@ -0,0 +1,90 @@
+var m = require('mithril');
+
+var Address = function(data) {
+  this.name = m.prop(data.name);
+  this.tel = m.prop(data.tel);
+};
+
+var vm = {
+  init: function() {
+    vm.name = m.prop('');
+    vm.tel = m.prop('');
+    vm.list = m.prop([]);
+    vm.list().push(new Address({
+      name: 'あいうえお',
+      tel: '123'
+    }));
+    vm.list().push(new Address({
+      name: 'かきくけこ',
+      tel: '456'
+    }));
+    vm.addAddress = function() {
+      if (vm.name() === '') return;
+      if (vm.tel() === '') return;
+      vm.list().push(new Address({
+        name: vm.name(),
+        tel: vm.tel()
+      }));
+      vm.name('');
+      vm.tel('');
+    };
+    vm.deleteAddress = function(index) {
+      vm.list().splice(index, 1);
+    }
+  }
+};
+
+var controller = function() {
+  vm.init();
+};
+
+var view = function() {
+  return m('div', [
+    m('div', [
+      m('input[type=text]', {
+        onchange: m.withAttr("value", vm.name),
+        placeholder: '名前',
+        value: vm.name()
+      }),
+      m('input[type=text]', {
+        onchange: m.withAttr("value", vm.tel),
+        placeholder: '電話番号',
+        value: vm.tel()
+      }),
+      m('input[type=button]', {
+        onclick: vm.addAddress.bind(vm),
+        value: '追加'
+      })
+    ]),
+    m('table', [
+      m('thead', [
+        m('tr', [
+          m('th', '名前'),
+          m('th', '電話番号'),
+          m('th')
+        ])
+      ]),
+      m('tbody', [
+        vm.list().map(function(address, index) {
+          return m('tr', [
+            m('td', address.name()),
+            m('td', address.tel()),
+            m('td', [
+              m('input[type=button]', {
+                onclick: function() {
+                  vm.deleteAddress(index)
+                },
+                value: '削除'
+              })
+            ])
+          ])
+        })
+      ])
+    ])
+  ]);
+};
+
+m.mount(document.getElementById('root'), {
+  controller: controller,
+  view: view
+});
