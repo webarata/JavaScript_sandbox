03115a6943554f5c636fae992eee45756540df1d
webarata3 <webarata3@gmail.com>
2015-12-04 05:34:52 +0900

    Mithrilの05を追加

---
+++ mithril_test/05-fileView/src/app.js
@@ -0,0 +1,47 @@
+// http://qiita.com/joe-re/items/483ddcbe3d04a0a1cebf
+// のJavaScript版
+
+var m = require('mithril');
+
+'use strict';
+
+var vm = {
+  init: function() {
+    vm.image = m.prop(null);
+    vm.fileChange = function(e) {
+      var file = e.target.files[0];
+      if (!file) {
+        vm.image(null);
+        return;
+      }
+      var fr = new FileReader();
+      fr.readAsDataURL(file);
+      m.startComputation();
+      fr.onload = function(event) {
+        vm.image(event.target.result);
+        m.endComputation();
+      };
+    }
+  }
+};
+
+var controller = function() {
+  vm.init();
+};
+
+
+var view = function() {
+  return m('div', [
+    m('input[type=file]', {
+      onchange: vm.fileChange
+    }),
+    m('img', {
+      src: vm.image()
+    })
+  ]);
+};
+
+m.mount(document.getElementById('root'), {
+  controller: controller,
+  view: view
+});
