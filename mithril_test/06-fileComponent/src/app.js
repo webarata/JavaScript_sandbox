90ce1831be76bd1aed662134e07f653ff5b55034
webarata3 <webarata3@gmail.com>
2016-01-02 19:29:04 +0900

    Mithril06の追加

---
+++ mithril_test/06-fileComponent/src/app.js
@@ -0,0 +1,59 @@
+// 参考
+// http://qiita.com/joe-re/items/483ddcbe3d04a0a1cebf
+
+var m = require('mithril');
+
+'use strict';
+
+var FileView = {};
+
+FileView.vm = {
+  init: function() {
+    FileView.vm.image = m.prop(null);
+    FileView.vm.fileChange = function(e) {
+      var file = e.target.files[0];
+      if (!file) {
+        FileView.vm.image(null);
+        return;
+      }
+      var fr = new FileReader();
+      fr.readAsDataURL(file);
+      m.startComputation();
+      fr.onload = function(event) {
+        FileView.vm.image(event.target.result);
+        m.endComputation();
+      };
+    }
+  }
+};
+
+FileView.controller = function() {
+  FileView.vm.init();
+};
+
+
+FileView.view = function() {
+  return m('div', [
+    m('input[type=file]', {
+      onchange: FileView.vm.fileChange
+    }),
+    m('img', {
+      src: FileView.vm.image(),
+      style: {
+        display: FileView.vm.image() ? 'inline-block' : 'none'
+      }
+    })
+  ]);
+};
+
+var myApp = {
+  controller: function() {},
+  view: function() {
+    return m('div', [
+      m.component(FileView),
+      m.component(FileView)
+    ]);
+  }
+};
+
+m.mount(document.getElementById('root'), myApp);
